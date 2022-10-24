3a3c1d7a631e61c87fadbd098be605870afbda53
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-10-24 21:38:09 +0300

    Add files via upload

---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Maxim Bekmetov on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
