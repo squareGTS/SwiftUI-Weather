65f17750ba07c51b563ab5359a12f9ac5411d615
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-10-25 15:26:11 +0300

    Main VStack w_ SF Symbol

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(.all)
@@ -27,8 +27,27 @@ struct ContentView: View {
                         .frame(width: 180, height: 180)
 
                     Text("76")
-                        .font(.system(size:70, weight: .medium))
+                        .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
+                }
+                .padding(.bottom, 40)
+
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeak: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 74)
+                    WeatherDayView(dayOfWeak: "WD",
+                                   imageName: "sun.max.fill",
+                                   temperature: 88)
+                    WeatherDayView(dayOfWeak: "THUR",
+                                   imageName: "wind.snow",
+                                   temperature: 55)
+                    WeatherDayView(dayOfWeak: "FRI",
+                                   imageName: "sunset.fill",
+                                   temperature: 60)
+                    WeatherDayView(dayOfWeak: "SAT",
+                                   imageName: "snow",
+                                   temperature: 25)
                 }
                 Spacer()
             }
@@ -39,5 +58,27 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    var dayOfWeak: String
+    var imageName: String
+    var temperature: Int
+
+    var body: some View {
+        VStack {
+            Text(dayOfWeak)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
